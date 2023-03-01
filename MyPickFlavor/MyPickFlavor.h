c00fdbcbe5099cb00d42ee74dd7cfb838f0b3f49
Henrique Batista <henrique.freitas.batista@sibs.com>
2023-03-01 16:38:30 +0000

    first

---
+++ MyPickFlavor/MyPickFlavor.h
@@ -0,0 +1,18 @@
+//
+//  MyPickFlavor.h
+//  MyPickFlavor
+//
+//  Created by Henrique Freitas Batista on 28/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPickFlavor.
+FOUNDATION_EXPORT double MyPickFlavorVersionNumber;
+
+//! Project version string for MyPickFlavor.
+FOUNDATION_EXPORT const unsigned char MyPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPickFlavor/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-MyPickFlavor/Pods-MyPickFlavor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyPickFlavor : NSObject
+@end
+@implementation PodsDummy_Pods_MyPickFlavor
+@end
